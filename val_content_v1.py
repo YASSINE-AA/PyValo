b94aab2c4013bc13ecbcc9445be648ed5eb3654d
Yassine Ahmed Ali <65458854+YASSINE-AA@users.noreply.github.com>
2023-01-05 15:09:18 +0100

    Update val_content_v1.py

--- val_content_v1.py
+++ val_content_v1.py
@@ -72,7 +72,7 @@ isActive	boolean
 
 
 class ContentAPI:
-	def __init__(self, REGION, API_KEY, LOCALE):
+	def __init__(self, REGION, API_KEY, LOCALE="en-US"):
 		self.response = requests.get(f"https://{REGION}.api.riotgames.com/val/content/v1/contents?locale={LOCALE}&api_key={API_KEY}")
 		self.response_content = j.loads(self.response.content)
 		self.response_codes = {"400":"Bad request","401":"Unauthorized","403":"Forbidden","404":"Data not found","405":"Method not allowed","415":"Unsupported media type","429":"Rate limit exceeded","500":"Internal server error","502":"Bad gateway","503":"Service unavailable","504":"Gateway timeout"}

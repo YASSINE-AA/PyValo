7b3a72412958360e0fb043a1467be54a4654d115
Yassine Ahmed Ali <65458854+YASSINE-AA@users.noreply.github.com>
2023-01-07 18:45:40 +0100

    Update request.py

--- valorant_apis/request.py
+++ valorant_apis/request.py
@@ -1,5 +1,7 @@
 import requests
 import json
+response_codes = {"400":"Bad request","401":"Unauthorized","403":"Forbidden","404":"Data not found","405":"Method not allowed","415":"Unsupported media type","429":"Rate limit exceeded","500":"Internal server error","502":"Bad gateway","503":"Service unavailable","504":"Gateway timeout"}
+
 
 # This class handles the requests sent to the Riot API
 class Request:
